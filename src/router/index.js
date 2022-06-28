8fcaedcb57aa8ed59c993a5de4ccee20e3ff5102
godgodwinter <kakadz.pro@gmail.com>
2022-06-29 02:03:48 +0700

    add profile and upload photo yayasan and detail photo per sekolah

--- src/router/index.js
+++ src/router/index.js
@@ -62,6 +62,11 @@ const router = createRouter({
           component: () => import("@/views/admin/DashBoard.vue"),
         },
         {
+          path: '/pages/settings/profile',
+          name: 'AdminSettingsProfile',
+          component: () => import("@/views/admin/settings/MyProfile.vue"),
+        },
+        {
           path: '/pages/sekolah',
           component: () => import("@/views/admin/sekolah/Index.vue"),
           name: 'AdminSekolah',
